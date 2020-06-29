315eaa636daae7bd2fcb7593237616e28e925e34
team-cake <eka.cheuk@gmail.com>
2020-06-29 13:38:59 +0200

    made repo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Class #42 is the best!
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Want to know more about Class #42?
         </a>
       </header>
     </div>
