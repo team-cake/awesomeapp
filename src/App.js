bd1ee45f70986f2853a47572a0d306da102ce4cf
team-cake <eka.cheuk@gmail.com>
2020-06-29 15:52:03 +0200

    reload

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Class #42 is the best!</p>
         <a
           className="App-link"
