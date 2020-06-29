af6b7453e72e649363148044636e99e72ef0f33e
team-cake <eka.cheuk@gmail.com>
2020-06-29 13:41:44 +0200

    changed apps.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          
         >
           Want to know more about Class #42?
         </a>
