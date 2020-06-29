11e3ded9b662b43adb6e9d8ab4728fd18a37e782
team-cake <eka.cheuk@gmail.com>
2020-06-29 20:18:16 +0200

    Generalizing the Pokemon content

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ function App() {
   return (
     <div>
       <main>
-        <Title content='Some simple title' />
+        <Title content="Some simple title" />
         <Pokemon
-          name='Charizard'
-          weight='90'
-          awesome='yes'
-          terrifying='no'
-          abilities='Blaze, Solar power, Tough claws, Drought'
+          name="Charizard"
+          weight="90"
+          awesome="yes"
+          terrifying="no"
+          abilities="Blaze, Solar power, Tough claws, Drought"
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight="6.9"
+          awesome="yes"
+          terrifying="no"
+          abilities="Overgrow, Chlorophyll"
+        />
+        <Pokemon
+          name="Mewtwo"
+          weight="112"
+          awesome="yes"
+          terrifying="yes"
+          abilities="Pressure, Unnerve, Steadfast, Insomnia"
+        />
+                <Pokemon
+          name='Mega beedrill'
+          weight='65'
+          awesome='no'
+          terrifying='yes'
+          abilities='Intimidate, Unnerve'
         />
       </main>
     </div>
