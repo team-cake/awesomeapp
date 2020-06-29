9d8f4eabb6f5d1750cbfe639d46a123bc67cdd8c
team-cake <eka.cheuk@gmail.com>
2020-06-29 15:27:36 +0200

    got rid of unnessecary files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Class #42 is the best!
-        </p>
+        <p>Class #42 is the best!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-
         >
           Wait what? Is Codaisseur the best in town?
         </a>
